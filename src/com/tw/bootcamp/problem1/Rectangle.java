1fe851ad1e08d8749e9c1cbd1b215d42795d586e
samirangharami <samirangharami05@gmail.com>
2026-05-07 11:46:44 +0530

    adds perimeter calculation method in rectangle class

--- src/com/tw/bootcamp/problem1/Rectangle.java
+++ src/com/tw/bootcamp/problem1/Rectangle.java
@@ -13,4 +13,8 @@ public class Rectangle {
     public int area() {
         return this.length * this.breadth;
     }
+
+    public int perimeter() {
+        return 2 * (this.length + this.breadth);
+    }
 }
--- test/com/tw/bootcamp/problem1/RectangleTest.java
+++ test/com/tw/bootcamp/problem1/RectangleTest.java
@@ -7,14 +7,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class RectangleTest {
 
     @Test
-    void shouldReturn10For2And5() {
+    void shouldReturnArea10For2And5() {
         Rectangle rectangle = new Rectangle(2, 5);
         assertEquals(10, rectangle.area());
     }
 
     @Test
-    void shouldReturn0ForLength0() {
+    void shouldReturnArea0ForLength0() {
         Rectangle rectangle = new Rectangle(0, 5000);
         assertEquals(0, rectangle.area());
+    }
+
+    @Test
+    void shouldReturnPerimeter18For5And4() {
+        Rectangle rectangle = new Rectangle(4, 5);
+        assertEquals(18, rectangle.perimeter());
     }
 }
