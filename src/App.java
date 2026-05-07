d5cd8cb29e6260d311bd6e3a2ad68b970bf3f8a1
samirangharami <samirangharami05@gmail.com>
2026-05-07 12:02:44 +0530

    implements square class with method area and perimeter

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
 import com.tw.bootcamp.problem1.Rectangle;
+import com.tw.bootcamp.problem1.Square;
 
 public class App {
     static void main() {
 
         Rectangle rectangle = new Rectangle(2,5);
         System.out.println(rectangle.area());
+        Square square = new Square(5);
     }
 }
---
+++ src/com/tw/bootcamp/problem1/Square.java
@@ -0,0 +1,17 @@
+package com.tw.bootcamp.problem1;
+
+public class Square {
+    private final int side;
+
+    public Square(int side) {
+        this.side = side;
+    }
+
+    public int area() {
+        return this.side * this.side;
+    }
+
+    public int perimeter() {
+        return 4 * this.side;
+    }
+}
