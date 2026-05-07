f17567437cfb56ea630bb4045876855a8f03126b
samirangharami <samirangharami05@gmail.com>
2026-05-07 17:21:43 +0530

    Enhances Probability with complement and union operations and improve test assertions

--- src/com/tw/bootcamp/problem2/domain/Probability.java
+++ src/com/tw/bootcamp/problem2/domain/Probability.java
@@ -1,7 +1,5 @@
 package com.tw.bootcamp.problem2.domain;
 
-import java.util.Objects;
-
 public class Probability {
 
     private final double value;
@@ -14,22 +12,22 @@ public class Probability {
         return new Probability(value);
     }
 
-    public static Probability not(double value) {
-        return new Probability(1 - value);
+    public static Probability not(Probability otherChance) {
+        return new Probability(1 - otherChance.value);
+    }
+
+    public Probability and(Probability otherChance) {
+        return new Probability(this.value * otherChance.value);
+    }
+
+    public Probability atleastOne(Probability otherChance) {
+        return new Probability((this.value + otherChance.value) - (this.value * otherChance.value));
+
     }
 
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Probability that)) return false;
         return Double.compare(value, that.value) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(value);
-    }
-
-    public Probability and(Probability otherChance) {
-        return new Probability(this.value * otherChance.value);
     }
 }
--- test/com/tw/bootcamp/problem2/domain/ChanceTest.java
+++ test/com/tw/bootcamp/problem2/domain/ChanceTest.java
@@ -9,13 +9,14 @@ class ChanceTest {
     @Test
     void shouldBeAbleToRepresentChanceOfTails() {
         Probability chanceOfGettingTails = Probability.of(0.25);
-        assertTrue(chanceOfGettingTails.equals(Probability.of(0.25)));
+        assertEquals(chanceOfGettingTails, Probability.of(0.25));
     }
 
     @Test
     void shouldBeAbleToRepresentChanceNotGettingOfTails() {
-        Probability chanceOfNotGettingTails = Probability.not(0.25);
-        assertTrue(chanceOfNotGettingTails.equals(Probability.of(0.75)));
+        Probability chanceOfGettingTails = Probability.of(0.25);
+        Probability chanceOfNotGettingTails = Probability.not(chanceOfGettingTails);
+        assertEquals(chanceOfNotGettingTails, Probability.of(0.75));
     }
 
     @Test
@@ -23,7 +24,20 @@ class ChanceTest {
         Probability chanceOfGettingTailsInCoin1 = Probability.of(0.25);
         Probability chanceOfGettingTailsInCoin2 = Probability.of(0.5);
         Probability chanceOfGettingTailsInTwoCoins = chanceOfGettingTailsInCoin1.and(chanceOfGettingTailsInCoin2);
-        assertTrue(chanceOfGettingTailsInTwoCoins.equals(Probability.of(0.125)));
+        assertEquals(chanceOfGettingTailsInTwoCoins, Probability.of(0.125));
     }
 
+    @Test
+    void shouldBeAbleToRepresentChanceOfGetting3InADice() {
+        Probability chanceOfGetting3InADice = Probability.of(0.167);
+        assertEquals(chanceOfGetting3InADice, Probability.of(0.167));
+    }
+
+    @Test
+    void shouldBeAbleToRepresentChanceOfGettingAtleastOneTailsInTwoCoins() {
+        Probability chanceOfGettingTailsInCoin1 = Probability.of(0.25);
+        Probability chanceOfGettingTailsInCoin2 = Probability.of(0.5);
+        Probability chanceOfGettingTailsInTwoCoins = chanceOfGettingTailsInCoin1.atleastOne(chanceOfGettingTailsInCoin2);
+        assertEquals(chanceOfGettingTailsInTwoCoins, Probability.of(0.625));
+    }
 }
